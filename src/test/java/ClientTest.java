f05eb3b1930c8782b94cd320b399be090e8bd044
Summer <sbrochtrup@gmail.com>
2015-08-28 16:37:12 -0700

    add equals method unit test

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -17,4 +17,25 @@ public class ClientTest {
     Client newClient = new Client("Marge Simpson", 1);
     assertEquals("Marge Simpson", newClient.getClientName());
   }
+
+  @Test
+  public void getStylstId_returnsStylistId_true() {
+    Client newClient = new Client("Marge Simpson", 1);
+    assertEquals(1, newClient.getStylistId());
+  }
+
+  @Test
+  public void save_newClientIsSavedIntoDatabaseWithId() {
+    Client newClient = new Client("Homer Simpson", 1);
+    newClient.save();
+    Client savedClient = Client.all().get(0);
+    assertEquals(newClient.getId(), savedClient.getId());
+  }
+
+  @Test
+  public void equals_returnsTrueIfClientNamesAreTheSame() {
+    Client client1 = new Client("Jerry Seinfeld", 1);
+    Client client2 = new Client("Jerry Seinfeld", 1);
+    assertTrue(client1.equals(client2));
+  }
 }
