5bb382f5dcf2eebcb79620f04863d0b2f454e70b
Summer <sbrochtrup@gmail.com>
2015-08-28 17:17:16 -0700

    Add route for add-stylist

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -18,5 +18,11 @@ public class App {
         model.put("template", "templates/index.vtl");
         return new ModelAndView(model, layout);
       }, new VelocityTemplateEngine());
+
+      get("/add-stylist", (request, response) -> {
+        HashMap<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/add-stylist.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
   }
 }
